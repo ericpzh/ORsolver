939bf176ec95b19b88e5c0faedbee368b157dc0f
ericpzh <23252925+ericpzh@users.noreply.github.com>
2017-11-01 16:15:15 -0500

    Update Transportation.py

--- Simplex/Transportation.py
+++ Simplex/Transportation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import copy, deepcopy
 '''
 In progress solver for transportation problem
 '''
@@ -27,15 +28,28 @@ matrix = np.concatenate((matrix,demand),axis=0)
 ##Takes in combine cost matrix
 ##return a combine initialize matrix
 def northwest(cost):
-  matrix = np.array([[]])
+  matrix = deepcopy(cost)
   for i in range(len(cost)):
     for j in range(len(cost[i])):
+      minnumber = min(cost[i][len(cost[i])-1],cost[len(cost)-1][j])
+      cost[i][j] = minnumber
+      if(cost[i][len(cost[i])-1] <= cost[len(cost)-1][j]):
+        for k in range(j,len(cost[i])):
+          cost[i][k] = "--"
+        i += 1
+      else:
+        for k in range(i,len(cost):
+          cost[k][j] = "|"
+        j += 1
+      cost[i][len(cost[i])-1] -= minnumber
+      cost[len(cost)-1][j] -= minnumber               
   return matrix
       
 ##Vogel's approx of initialization
 ##Takes in combine cost matrix
 ##return a combine initialize matrix
 def vogel(cost):
+  matrix = deepcopy(cost)
   
 ##solver function
 ##Takes in cost matrix, initialize matrix, bool debug(Print steps if True)
