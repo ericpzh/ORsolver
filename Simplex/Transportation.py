bfac2c7249214f0abe845247a73417fa098fee6c
ericpzh <23252925+ericpzh@users.noreply.github.com>
2017-11-01 12:30:20 -0500

    Update Transportation.py

--- Simplex/Transportation.py
+++ Simplex/Transportation.py
@@ -3,9 +3,27 @@ import numpy as np
 In progress solver for transportation problem
 '''
 ##Main input
-##input a transportation matrix
-matrix = np.array([
-  [],
-  [],
-  []
+##input costs,supply,demand of a transportation matrix
+## [Cost] [Supply]
+##[Demand]
+cost = np.array([
+  [16,16,13,22,17],
+  [14,14,13,19,15],
+  [19,19,20,23,99],
+  [99,0,99,0,0]
 ])
+supply = np.array([
+  [50],
+  [60],
+  [50],
+  [50]
+])
+demand = np.array([[30,20,70,30,60]])
+##concatenate the matrix together
+demand = np.concatenate((demand,[0]),axis=1)
+matrix = np.concatenate((cost,supply),axis=1)
+matrix = np.concatenate((matrix,demand),axis=0)
+##Northwest corner rule of initialization
+def northwest(matrix):
+##Vogel's approx of initialization
+def vogel(matrix):
