9cacc76adc33c967a0267a62fa698d83ed58a662
ericpzh <23252925+ericpzh@users.noreply.github.com>
2017-11-03 09:46:49 -0500

    Add files via upload

--- Simplex/Transportation.py
+++ Simplex/Transportation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from copy import copy, deepcopy
+
 '''
 In progress solver for transportation problem
 '''
@@ -8,61 +9,66 @@ In progress solver for transportation problem
 ## [Cost] [Supply]
 ##[Demand]
 cost = np.array([
-  [16,16,13,22,17],
-  [14,14,13,19,15],
-  [19,19,20,23,99],
-  [99,0,99,0,0]
+	[16, 16, 13, 22, 17],
+	[14, 14, 13, 19, 15],
+	[19, 19, 20, 23, 99],
+	[99, 0, 99, 0, 0]
 ])
 supply = np.array([
-  [50],
-  [60],
-  [50],
-  [50]
+	[50],
+	[60],
+	[50],
+	[50]
 ])
-demand = np.array([[30,20,70,30,60]])
+demand = np.array([[30, 20, 70, 30, 60]])
 ##concatenate the matrix together
-demand = np.concatenate((demand,[0]),axis=1)
-matrix = np.concatenate((cost,supply),axis=1)
-matrix = np.concatenate((matrix,demand),axis=0)
+demand = np.concatenate((demand, [0]), axis=1)
+matrix = np.concatenate((cost, supply), axis=1)
+matrix = np.concatenate((matrix, demand), axis=0)
+
+
 ##Northwest corner rule of initialization
 ##Takes in combine cost matrix
 ##return a combine initialize matrix
 def northwest(cost):
-  matrix = deepcopy(cost)
-  for i in range(len(cost)):
-    for j in range(len(cost[i])):
-      minnumber = min(cost[i][len(cost[i])-1],cost[len(cost)-1][j])
-      matrix[i][j] = minnumber
-      if(cost[i][len(cost[i])-1] <= cost[len(cost)-1][j]):
-        for k in range(j,len(cost[i])):
-          matrix[i][k] = "--"
-        i += 1
-      else:
-        for k in range(i,len(cost):
-          matrix[k][j] = "|"
-        j += 1
-      matrix[i][len(cost[i])-1] -= minnumber
-      matrix[len(cost)-1][j] -= minnumber               
-  return matrix
-      
+	matrix = deepcopy(cost)
+	for i in range(len(cost)):
+		for j in range(len(cost[i])):
+			minnumber = min(cost[i][len(cost[i]) - 1], cost[len(cost) - 1][j])
+			matrix[i][j] = minnumber
+			if (cost[i][len(cost[i]) - 1] <= cost[len(cost) - 1][j]):
+				for k in range(j, len(cost[i])):
+					matrix[i][k] = "--"
+				i += 1
+			else:
+				for k in range(i, len(cost)):
+					matrix[k][j] = "|"
+				j += 1
+				matrix[i][len(cost[i]) - 1] -= minnumber
+				matrix[len(cost) - 1][j] -= minnumber
+	return matrix
+
+
 ##Vogel's approx of initialization
 ##Takes in combine cost matrix
 ##return a combine initialize matrix
 def vogel(cost):
-  matrix = deepcopy(cost)
-  penaltycol, penaltyrow = [],[]
-  int i = 0
-  while(i < (len(cost)-1+len(cost[0])-1-1)):
-    for i in range(len(cost)):
-      penaltycol.append(np.amin(cost[cost != np.amin(cost[i]) and cost != cost[i][len(cost)-1]])
-    for j in range(len(cost[0])):
-      penaltyrow.append()
-                       
-  
+	matrix = deepcopy(cost)
+	penaltycol, penaltyrow = [], []
+	i = 0
+	while (i < (len(cost) - 1 + len(cost[0]) - 1 - 1)):
+		for i in range(len(cost)):
+			penaltycol.append(np.amin(cost[cost != np.amin(cost[i]) and cost != cost[i][len(cost) - 1]]))
+		cost = np.transpose(cost)
+		for i in range(len(cost)):
+			penaltyrow.append(np.amin(cost[cost != np.amin(cost[i]) and cost != cost[i][len(cost) - 1]]))
+		cost = np.transpose(cost)
+		i+=1
+	return matrix
+
 ##solver function
 ##Takes in cost matrix, initialize matrix, bool debug(Print steps if True)
 ##return matrix of optimal assignment
-def transportationSolve(cost,matrix,debug):
-  
+def transportationSolve(cost, matrix, debug):
+	return
 ##runit
-transportationSolve(northwest(matrix))
